67966975bf8f0cf31d0b8e05372b60fe41b5b983
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2026-04-12 22:45:17 -0500

    refactor: actualizar exportación de funciones en praxis/__init__.py
    Se actualiza la referencia a la función renombrada get_files (anteriormente get_files_recursively) para reflejar los cambios realizados en el módulo files.

--- praxis/__init__.py
+++ praxis/__init__.py
@@ -4,6 +4,6 @@ from . import log
 
 from .audio import convert_to_wav_pydub, needs_wav_conversion, probe_audio
 from .environment import load_environment_variables
-from .files import get_exclude_names, get_files_recursively
+from .files import get_exclude_names, get_files
 from .process import run_shell_command
 from .tar import compress_path_to_tar_gz
